45cda78e16d5e238151497ec0308501f7b95f2ca
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2022-12-10 12:50:53 -0500

    updated js code

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,4 @@
 
-
-
 var acceptLength = function()
 { //alphabates and special characters validation
   var formatStr = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
@@ -23,76 +21,3 @@ var acceptLength = function()
 
   return promptLength; // return user entered length
 };
-
-
-//Function to accept Character type from user and validate it 
-var acceptcharType = function()
-{ //Provide choice to user to select character type
-  var promptcharType = window.prompt("Please enter type of characters, select from following choices (1 or 2):\n"+
-                                      "1 : lowercase,uppercase characters and numbers\n"+
-                                      "2 : lowercase,uppercase characters, numbers and special characters ");
-  while (promptcharType===null)//If user select Cancel, come out of program
-  { 
-   return 1;
-  }
-
-  //Provide choice to user to select character types for password
-  switch (parseInt(promptcharType)){
-    case 1:
-      var promptPassStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-      return promptPassStr;
-      break;
-    case 2:
-      var promptPassStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-      return promptPassStr;
-      break;
-    default:
-      window.alert('You did not pick a valid option. Try again.');
-      return 0;
-      break;
-  }  
-};
-
-//Function to generate password if length and character type crieteria met by user
-var generatePassword = function(){
-  var generatedPass = "";
-  var passLength = acceptLength();
-  if (parseInt(passLength)!==1){
-
-      while (passLength===0){
-        passLength = acceptLength();
-      }
-  }
-  if (parseInt(passLength)!==1)
-  {
-    var passStr = acceptcharType();
-    if (parseInt(passStr)!==1)
-    {
-      while(passStr===0){
-        passStr = acceptcharType();
-      }
-      //Generate a random number to pick random character from charater type string
-      for (var cnt = 0; cnt < passLength; cnt++) { 
-        var getPos = Math.floor(Math.random() * passStr.length ); 
-        var generatedPass =  generatedPass + passStr.charAt(getPos);
-      }  
-    } 
-  } 
-return generatedPass; //return generated password string
-};
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  if (password !="")
-  {
-      var passwordText = document.querySelector("#password");
-      passwordText.value = password;
-  }
-};
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
